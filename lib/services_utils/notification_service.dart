13ad8fcd118b745a66a0900e5925656f92fdbb12
motsem_2010 <motsem.2010@gmail.com>
2026-06-21 10:22:55 +0300

    run with notifications

---
+++ lib/services_utils/notification_service.dart
@@ -0,0 +1,200 @@
+// import 'package:firebase_messaging/firebase_messaging.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+// @pragma('vm:entry-point')
+// class NotificationService {
+//   static Widget? teacherHomeView;
+//   static Widget? studentHomeView;
+//   static Widget? employeeHomeView;
+//   static Widget? parentHomeView;
+//   final flutterLocalNotificationsPlugin = new FlutterLocalNotificationsPlugin();
+//   static String? firebaseToken;
+//   static RemoteMessage? remoteMessage;
+//   static final NotificationService _notificationService =
+//       NotificationService._internal();
+
+//   factory NotificationService() {
+//     return _notificationService;
+//   }
+
+//   NotificationService._internal();
+
+//   Future<void> showNotification(
+//     int notificationId,
+//     String? notificationTitle,
+//     String? notificationContent,
+//     String payload, {
+//     String channelId = '0',
+//     String channelTitle = 'appName',
+//     String channelDescription = 'notification',
+//     Priority notificationPriority = Priority.high,
+//     Importance notificationImportance = Importance.max,
+//   }) async {
+//     var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
+//       channelId,
+//       channelTitle,
+//       playSound: true,
+//       channelShowBadge: true,
+//       importance: notificationImportance,
+//       priority: notificationPriority,
+//       icon: 'app_icon',
+//     );
+//     var iOSPlatformChannelSpecifics = new DarwinNotificationDetails();
+//     var platformChannelSpecifics = new NotificationDetails(
+//       android: androidPlatformChannelSpecifics,
+//       iOS: iOSPlatformChannelSpecifics,
+//     );
+//     await flutterLocalNotificationsPlugin.show(
+//       notificationId,
+//       notificationTitle,
+//       notificationContent,
+//       platformChannelSpecifics,
+//       payload: payload,
+//     );
+//   }
+
+//   @pragma('vm:entry-point')
+//   void start([BuildContext? context]) {
+//     var initializationSettingsAndroid = new AndroidInitializationSettings(
+//       'app_icon',
+//     );
+//     var initializationSettingsIOS = new DarwinInitializationSettings();
+//     var initializationSettings = new InitializationSettings(
+//       android: initializationSettingsAndroid,
+//       iOS: initializationSettingsIOS,
+//     );
+//     FirebaseMessaging.onMessageOpenedApp.listen((message) {
+//       if ((TokenDB.getAccessNotification() ?? 'false') == 'false') {
+//         remoteMessage = message;
+//         navigateNotification(remoteMessage);
+//         TokenDB.saveAccessNotification('true');
+//       }
+//     });
+//     FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
+//       if (Settings.currentUser == null) return;
+
+//       remoteMessage = message;
+//       int? notificationID;
+//       final String? navigationView = remoteMessage?.data['navigation'];
+//       if (navigationView == 'school_messages_view') {
+//         GeneralController.read(
+//           NavigatorKey.instance.currentContext!,
+//         ).getSchoolMessages();
+//       }
+//       if (navigationView != null &&
+//           navigationView.trim() != "" &&
+//           Routes.routes[navigationView] != null)
+//         if (navigationView != 'inbox_view' &&
+//             navigationView != 'school_messages_view')
+//           if (message.data['ID'] != null)
+//             notificationID =
+//                 int.tryParse(message.data['ID']) ?? remoteMessage.hashCode;
+//       showNotification(
+//         notificationID ?? message.hashCode,
+//         message.notification?.title ?? '',
+//         message.notification?.body ?? '',
+//         "${message.notification?.body}**${message.notification?.body}",
+//       );
+//       updateBadge(message);
+//       TokenDB.saveAccessNotification('false');
+//     });
+//     flutterLocalNotificationsPlugin.initialize(
+//       initializationSettings,
+//       onDidReceiveNotificationResponse: (payload) async {
+//         navigateNotification(remoteMessage);
+//         print('onDidReceiveNotificationResponse');
+//       },
+//     );
+//     FirebaseMessaging.instance.requestPermission(
+//       sound: true,
+//       badge: Main.withOutBadge,
+//       alert: true,
+//       provisional: false,
+//     );
+//   }
+
+//   Future<void> navigateNotification([RemoteMessage? remoteMessage]) async {
+//     if (Settings.currentUser == null) return;
+//     if (remoteMessage == null)
+//       remoteMessage =
+//           remoteMessage ?? await FirebaseMessaging.instance.getInitialMessage();
+
+//     if (remoteMessage != null) {
+//       var generalController = GeneralController.read(
+//         NavigatorKey.instance.currentContext!,
+//       );
+//       final String? navigationView = remoteMessage.data['navigation'];
+//       print(navigationView);
+//       final String messageID = (remoteMessage.data['ID'] ?? "0").toString();
+
+//       if (navigationView == 'parents_notes_view' ||
+//           navigationView == 'bus_delay' ||
+//           navigationView == 'birthday_view') {
+//         generalController.initBlockName = navigationView;
+//         if (navigationView == 'birthday_view') {
+//           generalController.isBirthDay = true;
+//         }
+//         if (navigationView == 'parents_notes_view' ||
+//             navigationView == 'bus_delay')
+//           generalController.notificationTitleAndMessage = (
+//             remoteMessage.notification?.title,
+//             remoteMessage.notification?.body,
+//           );
+//         debugPrint(remoteMessage.data.toString());
+
+//         try {
+//           if (AuthController.isParent(Settings.currentUser!.roles!))
+//             replace(parentHomeView!);
+//           else if (AuthController.isTeacher(Settings.currentUser!.roles!))
+//             replace(teacherHomeView!);
+//           else if (AuthController.isEmployee(Settings.currentUser!.roles!))
+//             replace(employeeHomeView!);
+//         } catch (e) {}
+//       }
+
+//       if (navigationView == 'achievements_and_notes_view') {
+//         var parentController = ParentController.read(
+//           NavigatorKey.instance.currentContext!,
+//         );
+//         parentController.studentDisciplineModelList = [];
+//       }
+
+//       if (navigationView == 'school_messages_view') {
+//         //   generalController.initBlockName = navigationView;
+
+//         return push(SchoolMessagesView(removeAppbar: false));
+//       }
+
+//       remoteMessage = null;
+//       if (navigationView != null &&
+//           navigationView.trim() != "" &&
+//           Routes.routes[navigationView] != null) {
+//         pushNamed(navigationView);
+//         if (Main.navigateToInboxDetails) generalController.inboxID = messageID;
+//       }
+//     }
+//   }
+
+//   static Future<void> updateBadge(RemoteMessage message) async {
+//     final String? navigationView = remoteMessage!.data['navigation'];
+//     if (navigationView != null &&
+//         navigationView.trim() != "" &&
+//         Routes.routes[navigationView] != null)
+//       if (navigationView == 'school_messages_view') {
+//         if (Main.schoolMessageBadge)
+//           await GeneralController.read(
+//             NavigatorKey.instance.currentContext!,
+//           ).getSchoolMsgCount();
+//       } else if (navigationView == 'inbox_view') {
+//         await GeneralController.read(
+//           NavigatorKey.instance.currentContext!,
+//         ).getMailBoxCount();
+//       }
+//   }
+
+//   void cancelNotification(int Id) {
+//     flutterLocalNotificationsPlugin.cancel(Id);
+//   }
+// }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,8 @@ import device_info_plus
 import file_picker
 import file_selector_macos
 import firebase_core
+import firebase_messaging
+import flutter_local_notifications
 import flutter_twain_scanner
 import path_provider_foundation
 import printing
@@ -22,6 +24,8 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterTwainScannerPlugin.register(with: registry.registrar(forPlugin: "FlutterTwainScannerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
